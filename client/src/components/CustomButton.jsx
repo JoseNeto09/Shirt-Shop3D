6a2ebce0b5f550bdd02ce549086ab41e67660671
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2023-04-19 10:22:56 -0300

    Api Key

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
-
-import state from '../store';
 import { useSnapshot } from 'valtio';
 
-const CustomButton = ({ type, title, customStyles, handleClick }) => {
-    const generateStyle = (type) => {
-        const snap = useSnapshot (state);
-        if(type === 'filled'){
-            return{
-                backgroundColor: snap.color,
-                color: '#fff'
-            }
-        }
-    }
+import state from '../store';
+import { getContrastingColor } from '../config/helpers';
 
-    return (
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  const generateStyle = (type) => {
+    if(type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: getContrastingColor(snap.color)
+      }
+    } else if(type === "outline") {
+      return {
+        borderWidth: '1px',
+        borderColor: snap.color,
+        color: snap.color
+      }
+    }
+  }
+
+  return (
     <button
-        className={`px-2 py-1.5 flex-1
-        rounded-md ${customStyles}`}
-        style={generateStyle(type)}
-        onClick={handleClick}
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
     >
-        {title}
+      {title}
     </button>
   )
 }
